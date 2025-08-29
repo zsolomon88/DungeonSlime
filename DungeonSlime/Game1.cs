9647f3ae9ed9a1cf198b526a8ed9b581397e2a39
zsolomon88 <zev2888@gmail.com>
2025-08-29 09:13:51 +0900

    Initial Commit

---
+++ DungeonSlime/Game1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+
+namespace DungeonSlime;
+
+public class Game1 : Core
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D _logo;
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
+    {
+
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+
+        // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/logo");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+        SpriteBatch.Begin();
+
+        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+
+        SpriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
